e0ab3fd0181e5c57fadac3c326e3d73385ca653c
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-01-29 09:23:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -305,8 +305,8 @@ async function starts() {
 					if (args.length < 2) return client.sendMessage(from, 'Cadê o texto tio', text, {quoted: mek})
 					dtt = body.slice(9)
 					ranm = getRandom('.mp3')
-					dtt.length > 6000
-					? reply('Texto muito grande')
+					dtt.length > 600
+					? reply('Texto muito grande.')
 					: gtts.save(ranm, dtt, function() {
 						client.sendMessage(from, fs.readFileSync(ranm), audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
 						fs.unlinkSync(ranm)
