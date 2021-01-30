75d9ab6f9c66bea30bf1b78b258f636d92131460
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-01-30 15:18:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const { help } = require('./src/help')
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson } = require('./lib/fetcher')
 const { recognize } = require('./lib/ocr')
-const { flahino } = require('./musics/flamengo')
 const fs = require('fs')
 const moment = require('moment-timezone')
 const { exec } = require('child_process')
@@ -23,6 +22,7 @@ const loli = new lolis()
 const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
+const flahino = JSON.parse(fs.readFileSync('./src/fla.json'))
 prefix = '.'
 blocked = []
 
@@ -222,8 +222,10 @@ async function starts() {
 		       			client.sendMessage(from, buffer, image, {quoted: mek, caption: `*Nome* : ${groupName}\n*Membro* : ${groupMembers.length}\n*Admin* : ${groupAdmins.length}`})
                 			break
 				case 'fla':
-					client.sendMessage(from, flahino, audio, {mimetype: 'audio/mp4', quoted: mek, ptt:true})
-                			break
+					anu = await fetchJson(`https://raw.githubusercontent.com/HiroshiJohn/Bot/main/src/fla.json', {method: 'get'})
+					buffer = await getBuffer(anu.url)
+					client.sendMessage(from, buffer, audio, {mimetype: 'audio/mp4', filename: `${anu.title}.mp3`, quoted: mek})
+					break
 				case 'nekonime':
           				data = await fetchJson('https://waifu.pics/api/sfw/neko')
            				hasil = await getBuffer(data.url)
