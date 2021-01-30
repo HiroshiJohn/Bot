4ae45c864cea2190efbbacc62d4d64b0187204aa
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-01-30 16:40:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -234,36 +234,35 @@ async function starts() {
            				client.sendMessage(from, hasil, image, {quoted: mek})
            				break
 				case 'waifu':
-					if (isDono == true) return reply('*Modo Deus ativado*, sem safadeza pra você.')
+					if ((isDono = true)) return reply('*Modo Deus ativado*, sem safadeza pra você.')
           				data = await fetchJson('https://waifu.pics/api/nsfw/waifu')
            				hasil = await getBuffer(data.url)
            				client.sendMessage(from, hasil, image, {quoted: mek})
            				break
 				case 'blowjob':
-					if (isDono == true) return reply('*Modo Deus ativado*, sem safadeza pra você.')
+					if ((isDono = true)) return reply('*Modo Deus ativado*, sem safadeza pra você.')
           				data = await fetchJson('https://waifu.pics/api/nsfw/blowjob')
            				hasil = await getBuffer(data.url)
            				client.sendMessage(from, hasil, image, {quoted: mek})
            				break
 				case 'neko':
-					if (isDono == true) return reply('*Modo Deus ativado*, sem safadeza pra você.')
+					if ((isDono = true)) return reply('*Modo Deus ativado*, sem safadeza pra você.')
           				data = await fetchJson('https://waifu.pics/api/nsfw/neko')
            				hasil = await getBuffer(data.url)
            				client.sendMessage(from, hasil, image, {quoted: mek})
            				break
 				case 'trap':
-					if (isDono == true) return reply('*Modo Deus ativado*, sem safadeza pra você.')
+					if ((isDono = true)) return reply('*Modo Deus ativado*, sem safadeza pra você.')
           				data = await fetchJson('https://waifu.pics/api/nsfw/trap')
            				hasil = await getBuffer(data.url)
            				client.sendMessage(from, hasil, image, {quoted: mek})
            				break
 				case 'god':
 					if (!isOwner) return reply('Você não é meu dono, saia daqui.')
-					if (isDono == false) {
+					if ((isDono = false)) {
 					isDono = true
 					reply('*Modo Deus Ativado*')
-					}
-					if (isDono == true) {
+					} else if ((isDono = true)) {
 					isDono = false 
 					reply('*Modo Deus Desativado*')
 					}
