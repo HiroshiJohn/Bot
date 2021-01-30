73bdbe380dc76eefe6b842878796c44b45d95163
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-01-30 16:18:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function starts() {
 			const isNsfw = isGroup ? nsfw.includes(from) : false
 			const isSimi = isGroup ? samih.includes(from) : false
 			const isOwner = ownerNumber.includes(sender)
-			const isDono = false
+			const isDono = isOwner ? : false
 			const isUrl = (url) => {
 			    return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
 			}
