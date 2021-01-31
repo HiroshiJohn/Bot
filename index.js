4620515823fd1f6efc30cb0af21a87577a3c6e1d
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-01-31 13:00:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -481,8 +481,7 @@ async function starts() {
 					if (args.length < 1) return reply('Onde está o url?')
 					anu = await fetchJson(`https://docs-jojo.herokuapp.com/api/yt-search?q=${args[0]}`, {method: 'get'})
 					if (anu.error) return reply(anu.error)
-					for (let i in anu.result) {
-						var item = anu.result[i];
+					for (let item of anu.result) {
 						teks = `*Title* : ${item.title}\n`
 					}
 					reply(teks)
