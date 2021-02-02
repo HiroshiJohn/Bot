06b5a0fe705498dadf27e8e983b399d33b8f49b6
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-02 19:14:55 -0300

    Create times.js

---
+++ src/times.js
@@ -0,0 +1,26 @@
+const times = (prefix) => {
+	return `
+
+Siglas dos times:
+
+cam = Atlético Mg
+cap = Atlético Pr
+bot = Botafogo
+cea = Ceará SC
+cor = Corinthians
+cfc = Coritiba
+fla = Flamengo
+flu = Fluminence
+for = Fortaleza
+goi = Goiás
+gre = Grêmio
+int = Internacional
+pal = Palmeiras
+san = Santos
+spf = São Paulo
+scr = Sport Club Recife
+
+`
+}
+
+exports.times = times
