325ea879c7eb3f11a2d9a13a21dd1adce06a127f
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-01 21:22:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -248,7 +248,12 @@ async function starts() {
                     			break
 				case 'fla':
 					anu = await fetchJson('https://raw.githubusercontent.com/HiroshiJohn/Bot/main/src/fla.json', {method: 'get'})
-					buffer = await getBuffer(anu.url)
+					buffer = await getBuffer(anu.fla)
+					client.sendMessage(from, buffer, audio, {mimetype: 'audio/mp4', quoted: mek})
+					break
+				case 'atleMG':
+					anu = await fetchJson('https://raw.githubusercontent.com/HiroshiJohn/Bot/main/src/fla.json', {method: 'get'})
+					buffer = await getBuffer(anu.atleMG)
 					client.sendMessage(from, buffer, audio, {mimetype: 'audio/mp4', quoted: mek})
 					break
 				case 'nekonime':
