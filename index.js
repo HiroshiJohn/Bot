db6591f625d4258027de6dc0e51ce16fe7c02417
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-08 20:44:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -429,7 +429,7 @@ async function starts() {
 					dtt.length > 600
 					? reply('Texto muito grande.')
 					: gtts.save(ranm, dtt, function() {
-						client.sendMessage(from, fs.readFileSync(ranm), audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
+						client.sendMessage(from, fs.readFileSync(ranm), audio, {quoted: mek, mimetype: 'audio/ogg', ptt:true})
 						fs.unlinkSync(ranm)
 					})
 					break
