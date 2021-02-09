d68234c051469ffe1d469d23e9d7fb33324fa59e
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-09 13:15:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -284,7 +284,6 @@ async function starts() {
 							.on('end', function () {
 								console.log('Finish')
 								client.sendMessage(from, fs.readFileSync(ran), video, {quoted: mek})
-								fs.unlinkSync(data.url)
 								fs.unlinkSync(ran)
 							})
 							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
