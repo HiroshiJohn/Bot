339bf15fc2fa8330f8bc8a306b2b53f90013de2e
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-10 12:55:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ async function starts() {
 							fs.unlinkSync('./blow.gif')
 							fs.unlinkSync(ran)
 							})
-						.addOutputOptions([`-c:v`, `libvpx`, `-crf 12`, `-b:v 500K`])
+						.addOutputOptions([`-movflags`, `faststart`, `-pix_fmt`, `yuv420p`, `-vf` `"scale=trunc(iw/2)*2:trunc(ih/2)*2"`])
 						.toFormat('mp4')
 						.save(ran)
            				break
