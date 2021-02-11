f840fddfc8bae7c1ff92e22e1a916795032c7f86
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-10 23:51:09 -0300

    Update times.js

--- src/times.js
+++ src/times.js
@@ -3,22 +3,28 @@ const times = (prefix) => {
 
 Siglas dos times:
 
+*Brasileirão Série A*
+
 cam = Atlético Mg
 cap = Atlético Pr
+cag = Atlético Go
 bot = Botafogo
 cea = Ceará SC
 cor = Corinthians
 cfc = Coritiba
 fla = Flamengo
-flu = Fluminence
+flu = Fluminense
 for = Fortaleza
 goi = Goiás
 gre = Grêmio
 int = Internacional
 pal = Palmeiras
+rbb = Red Bull Bragantino
 san = Santos
 spf = São Paulo
 scr = Sport Club Recife
+scb = Sport Club Bahia
+vas = Vasco da Gama
 
 `
 }
