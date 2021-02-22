b2301416f305897eecbe1084beff86d93b78c004
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-22 02:03:20 -0300

    Update fetcher.js

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,74 +1,97 @@
-const fetch = require ('node-fetch')
-const FormData = require ('form-data')
-const fs = require ('fs')
-const {fromBuffer} = requer ('tipo de arquivo')
-const resizeImage = require ('./ imageProcessing')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const fs = require('fs')
+const { fromBuffer } = require('file-type')
+const resizeImage = require('./imageProcessing')
 
-const fetchJson = (url, opções) =>
-    nova promessa ((resolver, rejeitar) =>
-        buscar (url, opções)
-            .então (resposta => resposta.json ())
-            .então (json => resolver (json))
-            .catch (err => {
-                console.error (err)
-                rejeitar (errar)
+/**
+ *Fetch Json from Url
+ *
+ *@param {String} url
+ *@param {Object} options
+ */
+
+const fetchJson = (url, options) =>
+    new Promise((resolve, reject) =>
+        fetch(url, options)
+            .then(response => response.json())
+            .then(json => resolve(json))
+            .catch(err => {
+                console.error(err)
+                reject(err)
             })
     )
 
-const fetchText = (url, opções) => {
-    retornar nova promessa ((resolver, rejeitar) => {
-        return fetch (url, opções)
-            .então (resposta => response.text ())
-            .então (texto => resolver (texto))
-            .catch (err => {
-                console.error (err)
-                rejeitar (errar)
+/**
+ * Fetch Text from Url
+ *
+ * @param {String} url
+ * @param {Object} options
+ */
+
+const fetchText = (url, options) => {
+    return new Promise((resolve, reject) => {
+        return fetch(url, options)
+            .then(response => response.text())
+            .then(text => resolve(text))
+            .catch(err => {
+                console.error(err)
+                reject(err)
             })
     })
 }
 
+/**
+ * Fetch base64 from url
+ * @param {String} url
+ */
 
 const fetchBase64 = (url, mimetype) => {
-    retornar nova promessa ((resolver, rejeitar) => {
-        console.log ('Obter base64 de:', url)
-        return fetch (url)
-            .então ((res) => {
-                const _mimetype = mimetype || res.headers.get ('content-type')
-                res.buffer ()
-                    .then ((result) => resolve (`data: $ {_ mimetype}; base64,` + result.toString ('base64')))
+    return new Promise((resolve, reject) => {
+        console.log('Get base64 from:', url)
+        return fetch(url)
+            .then((res) => {
+                const _mimetype = mimetype || res.headers.get('content-type')
+                res.buffer()
+                    .then((result) => resolve(`data:${_mimetype};base64,` + result.toString('base64')))
             })
-            .catch ((err) => {
-                console.error (err)
-                rejeitar (errar)
+            .catch((err) => {
+                console.error(err)
+                reject(err)
             })
     })
 }
 
-
+/**
+ * Upload Image to Telegra.ph
+ *
+ * @param  {String} base64 image buffer
+ * @param  {Boolean} resize
+ */
 
 const uploadImages = (buffData, type) => {
-    // eslint-disable-next-line no-async-promessa-executor
-    retornar nova promessa (assíncrona (resolver, rejeitar) => {
-        const {ext} = await fromBuffer (buffData)
-        const filePath = 'utils / tmp.' + ext
-        const _buffData = type? aguardar resizeImage (buffData, false): buffData
-        fs.writeFile (filePath, _buffData, {encoding: 'base64'}, (err) => {
-            if (err) retornar rejeitar (err)
-            console.log ('Enviando imagem para o servidor telegra.ph ...')
-            const fileData = fs.readFileSync (filePath)
-            const form = new FormData ()
-            form.append ('arquivo', arquivoData, 'tmp.' + ext)
-            fetch ('https://telegra.ph/upload', {
-                método: 'POST',
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async (resolve, reject) => {
+        const { ext } = await fromBuffer(buffData)
+        const filePath = 'utils/tmp.' + ext
+        const _buffData = type ? await resizeImage(buffData, false) : buffData
+        fs.writeFile(filePath, _buffData, { encoding: 'base64' }, (err) => {
+            if (err) return reject(err)
+            console.log('Uploading image to telegra.ph server...')
+            const fileData = fs.readFileSync(filePath)
+            const form = new FormData()
+            form.append('file', fileData, 'tmp.' + ext)
+            fetch('https://telegra.ph/upload', {
+                method: 'POST',
                 body: form
             })
-                .então (res => res.json ())
-                .então (res => {
-                    if (res.error) retorna rejeitar (res.error)
-                    resolver ('https://telegra.ph' + res [0] .src)
+                .then(res => res.json())
+                .then(res => {
+                    if (res.error) return reject(res.error)
+                    resolve('https://telegra.ph' + res[0].src)
                 })
-                .então (() => fs.unlinkSync (filePath))
-                .catch (errar => rejeitar (errar))
+                .then(() => fs.unlinkSync(filePath))
+                .catch(err => reject(err))
         })
     })
 }
@@ -77,5 +100,5 @@ module.exports = {
     fetchJson,
     fetchText,
     fetchBase64,
-    Enviar imagens
+    uploadImages
 }
