20fcd4ce56bb5949417c7e8d84cd6082dcd28a0d
John Vitor <77373022+HiroshiJohn@users.noreply.github.com>
2021-02-22 12:24:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -184,10 +184,11 @@ async function starts() {
 			if (!isGroup && !isCmd) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;31mRECV\x1b[1;37m]', time, color('Message'), 'from', color(sender.split('@')[0]), 'args :', color(args.length))
 			if (isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(command), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
 			if (!isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;31mRECV\x1b[1;37m]', time, color('Message'), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
-			if (isBanned) return reply('Você está banido!')
+			if (isBanned) return console.log(color('[BAN]', 'red'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname))
 			switch(command) {
 				case 'help':
 				case 'menu':
+					if (isBanned) return reply('Você está banido2!')
 					client.sendMessage(from, help(prefix), text)
 					break
 				case 'leaveall':
